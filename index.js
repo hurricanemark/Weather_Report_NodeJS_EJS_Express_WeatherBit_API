defb788640d6775e35677980b10520a2ae356c50
markn <markn@aesclever.com>
2022-10-02 16:11:20 -0700

    modified:   index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // EJS is accessed by default in the views directory.
 app.set('view engine', 'ejs');
 
-// Allow access to 'public' folder
+// Allow access to 'public' folder where resources are available to this app
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +27,7 @@ app.post('/', (req, res) => {
     let city = req.body.locale;
     let url = process.env.WEATHER_VISUALCROSSING_API_BASE_URI;
     
-    /* https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Fremont%2C%20CA?unitGroup=us&contentType=json&key=apiKey */
+    /* https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&contentType=json&key=${apiKey} */
     let uriStr = `${url}${city}?unitGroup=us&contentType=json&key=${apiKey}`;
     console.log(uriStr);
     request(uriStr, async function (err, response, body) {
@@ -35,16 +35,14 @@ app.post('/', (req, res) => {
         console.log('error:', err);
       } else {
         let weather = await JSON.parse(body).currentConditions;
-        console.log(weather);
-
-        // res.render('index', {locale: city, data: weather, error: null});
+        // console.log(weather);
 
         if (weather.datetime == undefined) {
             console.log("Something went wrong.  No weather data.");
             res.render('index', { data: null, error: 'Error, please check your input.'});
         } else {
-            console.log("currentConditions:\n");
-            console.log(weather);
+            // console.log("currentConditions:\n");
+            // console.log(weather);
             res.render('index', { locale: city ,data: weather, error: null });
         }
       }
