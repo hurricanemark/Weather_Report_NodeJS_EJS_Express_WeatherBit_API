c743e6ea69a9b5f768aebbce5fc6458029b9aada
markn <markn@aesclever.com>
2022-11-15 17:24:37 -0800

    Clean up cluters in `index.js` to resemble a lean application diver

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import request from 'request';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {encryptAES, decryptAES} from './crypto.js';
-import { data as currencyCodes } from 'currency-codes';
-import awssdk from 'aws-sdk';
+
 
 // application secrets
 import { keys } from './config/keys.js';  
@@ -15,25 +14,8 @@ import { router as currencyExchangeRoutes } from './routes/currency-exchange-rou
 // routes handler for WeatherBit.io API methods
 import { router as weatherbitRoutes } from './routes/weatherbit-routes.js';
 
-let WEATHERBIT_KEY = "";
-let WEATHERBIT_URI = "";
-
+// dev env
 dotenv.config();
-
-if (process.env.NODE_ENV === 'awsdeploy') {
-  WEATHERBIT_URI="https://api.weatherbit.io/v2.0/";
-  WEATHERBIT_KEY="U2FsdGVkX18HMV5UUT9rJN76hOtIHDw1bH0beQYWH8a6E7uzKqskdgHvc6Nq2lO6O+GAb2vrcL+X8ZDqcGPuLw==";
-  console.log("AWSDEPLOY mode!");
-} else {
-  // Code for Development Mode
-  // WEATHERBIT_KEY = process.env.WEATHERBIT_KEY;
-  // WEATHERBIT_URI = process.env.WEATHERBIT_URI;
-
-  WEATHERBIT_KEY = keys.weatherbitapi.APIKEY;
-  WEATHERBIT_URI = keys.weatherbitapi.URI;
-}
-
-
 
 // Create network routing
 const app = express();
@@ -55,8 +37,7 @@ app.use('/weatherbit', weatherbitRoutes);
 
 // Homepage: get the locale from the client-side via the ejs form
 app.get('/', (req, res) => {
-  let apikey = encryptAES(WEATHERBIT_KEY);
-  res.render('index', {xkey: apikey});
+  res.render('index', {xkey: 'hey'});
 })
 
 // about page
